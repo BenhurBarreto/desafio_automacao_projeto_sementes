055c20c091665dc1002e8da3fae72d758dd6df1a
Benhur <benhurbbs@gmail.com>
2024-07-20 20:27:39 -0300

    Configuração básica do projeto

---
+++ AutomacaoSementes/src/test/java/steps/CompraSteps.java
@@ -0,0 +1,65 @@
+package steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.E;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
+
+public class CompraSteps {
+
+    @Dado("que acesso a Product Store")
+    public void que_acesso_a_product_store() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @E("acesso a categoria Monitors")
+    public void acesso_a_categoria_monitors() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @E("seleciono um Apple Monitor {int}")
+    public void seleciono_um_apple_monitor(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @E("o adiciono o produto no carrinho Chart")
+    public void o_adiciono_o_produto_no_carrinho_chart() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @E("confirmo clicando em OK na caixa de diálogo que aparece")
+    public void confirmo_clicando_em_ok_na_caixa_de_diálogo_que_aparece() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @E("acesso o carrinho em Chart")
+    public void acesso_o_carrinho_em_chart() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @E("confirmo a inclusão")
+    public void confirmo_a_inclusão() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Quando("clico em Place Order")
+    public void clico_em_place_order() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @E("preencho todos os campos")
+    public void preencho_todos_os_campos() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @E("clico em Purchase")
+    public void clico_em_purchase() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Então("eu devo ver a mensagem de compra realizada")
+    public void eu_devo_ver_a_mensagem_de_compra_realizada() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
---
+++ AutomacaoSementes/src/test/resources/features/Compra.feature
@@ -0,0 +1,16 @@
+#language:pt
+
+Funcionalidade: Compra de Monitor
+
+Cenário: Realiza compra de Monitor
+  Dado que acesso a Product Store
+  E acesso a categoria Monitors
+  E seleciono um Apple Monitor 24
+  E o adiciono o produto no carrinho Chart
+  E confirmo clicando em OK na caixa de diálogo que aparece
+  E acesso o carrinho em Chart
+  E confirmo a inclusão
+  Quando clico em Place Order
+  E preencho todos os campos
+  E clico em Purchase
+  Então eu devo ver a mensagem de compra realizada
